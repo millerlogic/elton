a1221a75a13f77e454c9594e0ebb29a2fce2e45c
millerlogic <chris@cmiller.me>
2014-06-28 13:29:05 -0700

    Turned into a module. Split up file and string functions. Use safe env.

--- lua/serializer.lua
+++ lua/serializer.lua
@@ -2,6 +2,8 @@
 -- Dual License (choose): MIT License 1.0 / Boost Software License 1.0
 -- http://www.boost.org/users/license.html
 
+local M = {}
+
 
 local entry -- forward decl
 
@@ -57,23 +59,12 @@ function entry(k, v, level, file)
 end
 
 
--- obj is the object to serialize.
--- file can be a filename, a file object, or any object with a :write(...) method.
--- if file is nil or omitted, serializes to a string returned.
-function serialize(obj, file)
+--- obj is the object to serialize.
+--- file can be a filename, a file object, or any object with a :write(...) method.
+function M.serialize(obj, file)
 	local isstr = false
 	local needclose = false
-	if not file then
-		file = { str = "" }
-		file.write = function(self, ...)
-			local n = select('#', ...)
-			for i = 1, n do
-				local x = select(i, ...)
-				self.str = self.str .. x
-			end
-		end
-		isstr = true
-	elseif type(file) == "string" then
+	if type(file) == "string" then
 		local f, xerr = io.open(file, "w+") -- update mode, all previous data is erased
 		if not f then return nil, xerr end
 		file = f
@@ -90,60 +81,123 @@ function serialize(obj, file)
 end
 
 
--- source can be a filename, a file object,
--- any object with a :read() method,
--- or a string returned from serialize.
--- Warning: any code will be executed!
-function deserialize(source)
-	local file
-	local tt = "file"
-	local st = 0
-	local needclose = false
-	if type(source) == "string" then
-		if source:find("-- serializer 8906\n", 1, true) then
-			local fn, xerr = load(function()
-				st = st + 1
-				if st == 1 then
-					return "return {\n"
-				elseif st == 2 then
-					return source
-				elseif st == 3 then
-					return "\n}"
-				end
-			end, "deserialize-string")
-			if not fn then return nil, xerr end
-			return fn()
-		else
-			local f, xerr = io.open(source, "r")
-			if not f then return nil, xerr end
-			file = f
-			needclose = true
-			tt = "filename-" .. source
+---
+function M.stringify(obj)
+	local file = { }
+	function file:write(...)
+		local n = select('#', ...)
+		for i = 1, n do
+			local x = select(i, ...)
+			self[#self + 1] = x
 		end
 	end
-	local fn, xerr = load(function()
-		if st == 0 then
-			st = st + 1
-			return "return {\n"
-		elseif st == 1 then
-			local rr = file:read(4096)
-			if rr then return rr end
-			st = st + 1
-			return "\n}"
-		end
-	end, "deserialize-" .. tt)
-	if needclose then file:close() end
-	if not fn then return nil, xerr end
-	return fn()
+	local a, b = M.serialize(obj, file)
+	if not a then
+		return a, b
+	end
+	return table.concat(file)
 end
 
 
-function serializer_Test()
+local function loadfunc(ld, name, env)
+	if _VERSION == "Lua 5.1" then
+		local a, b = load(ld, name)
+		if not a then
+			return a, b
+		end
+		setfenv(a, env)
+		return a
+	elseif _VERSION >= "Lua 5.2" then
+		return load(ld, name, "t", env)
+	end
+end
+
+
+local function validate(obj)
+	for k, v in pairs(obj) do
+		local kt = type(k)
+		local vt = type(v)
+		assert(kt ~= "function" and vt ~= "function", "Function not allowed")
+		if kt == "table" then
+			validate(v)
+		end
+		if vt == "table" then
+			validate(v)
+		end
+	end
+end
+
+
+local function deserializer(source, isLiteral)
+	local name = "file"
+	local file = source
+	local needclose = false
+	if type(source) == "string" then
+		if isLiteral then
+			name = "data"
+		else
+			local f, xerr = io.open(source, "rb")
+			if not f then return nil, xerr end
+			file = f
+			needclose = true
+			name = "filename-" .. source
+		end
+	end
+	local env = { }
+	local state = 0
+	local fn, xerr = loadfunc(function()
+		if state == 0 then
+			state = 1
+			return "return {\n"
+		elseif state == 1 then
+			if isLiteral then
+				state = 2
+				return source
+			end
+			local rr = file:read(4096)
+			if rr then
+				return rr
+			end
+			state = -1
+			return "\n}"
+		elseif state == 2 then
+			-- For isLiteral only.
+			state = -1
+			return "\n}"
+		end
+	end, "deserialize-" .. name, env)
+	if needclose then file:close() end
+	if not fn then return nil, xerr end
+	local a, b = fn()
+	if a and b then
+		validate(a)
+		setmetatable(b, a)
+		a, b = b, nil
+	end
+	assert(type(a) == "table", "Table expected")
+	return a
+end
+
+
+--- source can be a filename, a file object, or any object with a :read() method.
+function M.deserialize(source)
+	return deserializer(source)
+end
+
+
+--- data is literally serialized data.
+function M.parse(data)
+	return deserializer(data, true)
+end
+
+
+--[[
+local function serializer_Test()
 	local t = { 33, hello = 3, [99] = 99, ["list!"] = { 5, 4, 3, 2, 1 },
 		["x*x"] = "x\"'''\n", mybool = true, ["your-bool"] = false }
-	local s = assert(serialize(t))
+	local s = assert(M.stringify(t))
 	print("serialize = `" .. s .. "`")
-	local t2 = deserialize(s)
+	local t2 = assert(M.parse(s))
 	for k, v in pairs(t) do
 		if type(v) ~= "table" then
 			assert(t2[k] == v)
@@ -152,5 +206,9 @@ function serializer_Test()
 	print("serializer_Test PASS")
 end
 
--- serializer_Test() -- Run test.
+serializer_Test() -- Run test.
+--]]
+
+
+return M
 
